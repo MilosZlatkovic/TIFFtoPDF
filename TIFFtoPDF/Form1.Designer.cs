2adb655ecd7ba29746f2d2a5876155e48bdd5187
Milos Zlatkovic <milos_zlatkovic@ymail.com>
2014-07-17 22:17:00 +0200

    first commit

---
+++ TIFFtoPDF/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TIFFtoPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.select_button = new System.Windows.Forms.Button();
+            this.convert_button = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.remove_button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.up_button = new System.Windows.Forms.Button();
+            this.down_button = new System.Windows.Forms.Button();
+            this.sort_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // select_button
+            // 
+            this.select_button.Location = new System.Drawing.Point(12, 11);
+            this.select_button.Name = "select_button";
+            this.select_button.Size = new System.Drawing.Size(70, 30);
+            this.select_button.TabIndex = 1;
+            this.select_button.Text = "SELECT";
+            this.select_button.UseVisualStyleBackColor = true;
+            this.select_button.Click += new System.EventHandler(this.select_button_Click);
+            // 
+            // convert_button
+            // 
+            this.convert_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.convert_button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.convert_button.FlatAppearance.BorderSize = 0;
+            this.convert_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.convert_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convert_button.ForeColor = System.Drawing.Color.White;
+            this.convert_button.Location = new System.Drawing.Point(502, 227);
+            this.convert_button.Name = "convert_button";
+            this.convert_button.Size = new System.Drawing.Size(70, 30);
+            this.convert_button.TabIndex = 2;
+            this.convert_button.Text = "CONVERT";
+            this.convert_button.UseVisualStyleBackColor = false;
+            this.convert_button.Visible = false;
+            this.convert_button.Click += new System.EventHandler(this.convert_button_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(502, 47);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(70, 30);
+            this.clear_button.TabIndex = 3;
+            this.clear_button.Text = "CLEAR";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // remove_button
+            // 
+            this.remove_button.Location = new System.Drawing.Point(502, 83);
+            this.remove_button.Name = "remove_button";
+            this.remove_button.Size = new System.Drawing.Size(70, 30);
+            this.remove_button.TabIndex = 4;
+            this.remove_button.Text = "REMOVE";
+            this.remove_button.UseVisualStyleBackColor = true;
+            this.remove_button.Click += new System.EventHandler(this.remove_button_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.Red;
+            this.label.Location = new System.Drawing.Point(135, 15);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 16);
+            this.label.TabIndex = 5;
+            // 
+            // up_button
+            // 
+            this.up_button.Location = new System.Drawing.Point(502, 119);
+            this.up_button.Name = "up_button";
+            this.up_button.Size = new System.Drawing.Size(70, 30);
+            this.up_button.TabIndex = 6;
+            this.up_button.Text = "˄";
+            this.up_button.UseVisualStyleBackColor = true;
+            this.up_button.Click += new System.EventHandler(this.up_button_Click);
+            // 
+            // down_button
+            // 
+            this.down_button.Location = new System.Drawing.Point(502, 155);
+            this.down_button.Name = "down_button";
+            this.down_button.Size = new System.Drawing.Size(70, 30);
+            this.down_button.TabIndex = 7;
+            this.down_button.Text = "˅";
+            this.down_button.UseVisualStyleBackColor = true;
+            this.down_button.Click += new System.EventHandler(this.down_button_Click);
+            // 
+            // sort_button
+            // 
+            this.sort_button.Location = new System.Drawing.Point(502, 191);
+            this.sort_button.Name = "sort_button";
+            this.sort_button.Size = new System.Drawing.Size(70, 30);
+            this.sort_button.TabIndex = 9;
+            this.sort_button.Text = "SORT";
+            this.sort_button.UseVisualStyleBackColor = true;
+            this.sort_button.Click += new System.EventHandler(this.sort_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 268);
+            this.Controls.Add(this.sort_button);
+            this.Controls.Add(this.down_button);
+            this.Controls.Add(this.up_button);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.remove_button);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.convert_button);
+            this.Controls.Add(this.select_button);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TIFF 2 PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button select_button;
+        private System.Windows.Forms.Button convert_button;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Button remove_button;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button up_button;
+        private System.Windows.Forms.Button down_button;
+        private System.Windows.Forms.Button sort_button;
+    }
+}
+
---
+++ TIFFtoPDF/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Drawing.Imaging;
+
+namespace TIFFtoPDF
+{
+    public partial class Form1 : Form
+    {
+        List<String> filenames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTiff2Pdf(string tiffFileName, ref PdfWriter writer, ref Document document)
+        {
+
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(tiffFileName);
+
+            int numberOfPages = bitmap.GetFrameCount(System.Drawing.Imaging.FrameDimension.Page);
+
+            PdfContentByte cb = writer.DirectContent;
+
+            for (int page = 0; page < numberOfPages; page++)
+            {
+
+                bitmap.SelectActiveFrame(System.Drawing.Imaging.FrameDimension.Page, page);
+
+                System.IO.MemoryStream stream = new System.IO.MemoryStream();
+
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(stream.ToArray());
+
+                stream.Close();
+
+                img.ScalePercent(72f / bitmap.HorizontalResolution * 100);
+
+                img.SetAbsolutePosition(0, 0);
+
+                cb.AddImage(img);
+
+                document.NewPage();
+            }
+        }
+
+        private void select_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            listBox1.Items.Clear();
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Select TIF files";
+            theDialog.Filter = "TIF files|*.tif";          
+
+            // Opening multiple files
+            theDialog.Multiselect = true;
+            
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    foreach (String file in theDialog.FileNames)
+                    {
+                        listBox1.Items.Add(file);
+                        filenames.Add(file);
+                    }
+
+                    convert_button.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void convert_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "PDF file|*.pdf";
+            sfd.FileName = DateTime.Now.ToString("yyyy-MM-dd HH_mm_ss_tt");           
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+
+                PdfWriter writer = PdfWriter.GetInstance(document,
+                                                         new FileStream(sfd.FileName,
+                                                         FileMode.CreateNew));
+                document.Open();
+
+                foreach (String file in filenames)
+                {
+                    AddTiff2Pdf(file, ref writer, ref document);
+                }
+
+                document.Close();
+
+                listBox1.Items.Clear();
+                filenames.Clear();
+                listBox1.Items.Add("Done.");
+                convert_button.Visible = false;
+            }
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            listBox1.Items.Clear();
+            filenames.Clear();
+            convert_button.Visible = false;
+        }
+
+        private void remove_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            try
+            {
+                // Get the currently selected item in the ListBox. 
+                string curItem = listBox1.SelectedItem.ToString();
+
+                listBox1.Items.Remove(curItem);
+                filenames.Remove(curItem);
+            }
+            catch (NullReferenceException)
+            {
+                label.Text = "To remove item, you must first select it from the list.";
+            }
+
+            if (filenames.Count == 0)
+                convert_button.Visible = false;
+        }
+
+        private void up_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            try
+            {
+                // Get the currently selected item in the ListBox. 
+                string curItem = listBox1.SelectedItem.ToString();
+                
+                int curItemIndex = listBox1.Items.IndexOf(curItem);
+
+                if (curItemIndex != 0)
+                {
+                    string temp = listBox1.Items[curItemIndex - 1].ToString();
+                    listBox1.Items[curItemIndex - 1] = curItem;
+                    listBox1.Items[curItemIndex] = temp;
+                    listBox1.SelectedItems.Clear();
+                    listBox1.SetSelected(curItemIndex - 1, true);
+
+                    filenames[curItemIndex] = temp;
+                    filenames[curItemIndex - 1] = curItem;                    
+                }
+            }
+            catch (NullReferenceException)
+            {
+                label.Text = "To move item, you must first select it from the list.";
+            }
+        }
+
+        private void down_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+            try
+            {
+                // Get the currently selected item in the ListBox. 
+                string curItem = listBox1.SelectedItem.ToString();
+
+                int curItemIndex = listBox1.Items.IndexOf(curItem);
+
+                if (curItemIndex != filenames.Count - 1)
+                {
+                    string temp = listBox1.Items[curItemIndex + 1].ToString();
+                    listBox1.Items[curItemIndex + 1] = curItem;
+                    listBox1.Items[curItemIndex] = temp;
+                    listBox1.SelectedItems.Clear();
+                    listBox1.SetSelected(curItemIndex + 1, true);
+
+                    filenames[curItemIndex] = temp;
+                    filenames[curItemIndex + 1] = curItem;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                label.Text = "To move item, you must first select it from the list.";
+            }
+        }
+
+        private void sort_button_Click(object sender, EventArgs e)
+        {
+            label.Text = "";
+
+            if (filenames.Count() != 0)
+            {
+                filenames.Sort();
+                listBox1.Items.Clear();
+
+                foreach (String file in filenames)
+                    listBox1.Items.Add(file);
+            }
+            else label.Text = "You must first insert items to the list.";
+        }
+    }
+}
